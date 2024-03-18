2e0b9a22c35fc29b75b42936128a19ecc50498f8
Yash Sutar <97961881+yash0865@users.noreply.github.com>
2024-03-18 20:13:49 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("http://localhost:5000", {
+            const socket = io("https://chat-app-mern-zwn3.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
