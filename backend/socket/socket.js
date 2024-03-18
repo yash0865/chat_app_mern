6cb1f889470535b335c35559b338310c7409b1c0
Yash Sutar <97961881+yash0865@users.noreply.github.com>
2024-03-18 20:03:25 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:3000"],
+        origin: ["https://chat-app-mern-zwn3.onrender.com"],
         methods: ["GET", "POST"],
     },
 });
